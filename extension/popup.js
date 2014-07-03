41dfc603ce295c7c471dd6a587940dc5e6ade34b
aicioara <primulandrei@yahoo.com>
2014-07-02 22:22:34 -0700

    Added enhancements to the popup

--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,17 @@
 chrome.tabs.executeScript(null, {
 	file: 'injection.js'
 });
+
+var hostId = document.getElementById('host-id');
+hostId.value = "12345245235475";
+hostId.onclick = function() {
+	this.select();
+}
+
+
+var guestId = document.getElementById('guest-id');
+var guestButton = document.getElementById('guest-button');
+
+guestButton.onclick = function() {
+	window.close();
+}
