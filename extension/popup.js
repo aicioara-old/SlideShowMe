089544f3e72a41591cfec3f112a7ee380e1dc811
aicioara <primulandrei@yahoo.com>
2014-07-02 23:56:52 -0700

    popup changes colour

--- extension/popup.js
+++ extension/popup.js
@@ -9,7 +9,7 @@ hostId.click(function() {
 });
 hostId.keydown(function(e) {
 	if (e.keyCode == 67 && e.ctrlKey) {
-		window.close();
+		onHostSelected();
 	}
 })
 
@@ -18,7 +18,45 @@ var guestId = $('#guest-id');
 var guestButton = $('#guest-button');
 
 guestButton.click(function() {
-	window.close();
+	onGuestSelected();
 });
 
 
+onGuestSelected = function() {
+
+	getCurrentTab(function(id) {
+		chrome.browserAction.setIcon({
+			path: "icon_blue.png",
+			tabId: id
+		}, function() {
+			window.close();
+		});
+
+	});
+
+}
+
+onHostSelected = function() {
+
+	getCurrentTab(function(id) {
+		chrome.browserAction.setIcon({
+			path: "icon_green.png",
+			tabId: id
+		}, function() {
+			window.close();
+
+		});
+
+	});
+
+}
+
+
+function getCurrentTab(callback) {
+	chrome.tabs.query(
+		{ currentWindow: true, active: true },
+		function (tabArray) {
+			callback(tabArray[0].id);
+		}
+	);
+}
