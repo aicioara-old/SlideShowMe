60a5b7f50f70670459809a89f3d7a2cab2ebd7ec
aicioara <primulandrei@yahoo.com>
2014-07-02 22:25:05 -0700

    Using jQuery in the popup

--- extension/popup.js
+++ extension/popup.js
@@ -2,16 +2,16 @@ chrome.tabs.executeScript(null, {
 	file: 'injection.js'
 });
 
-var hostId = document.getElementById('host-id');
-hostId.value = "12345245235475";
-hostId.onclick = function() {
+var hostId = $("#host-id");
+hostId.val("12345245235475");
+hostId.click(function() {
 	this.select();
-}
+});
 
 
-var guestId = document.getElementById('guest-id');
-var guestButton = document.getElementById('guest-button');
+var guestId = $('#guest-id');
+var guestButton = $('#guest-button');
 
-guestButton.onclick = function() {
+guestButton.click(function() {
 	window.close();
-}
+});
