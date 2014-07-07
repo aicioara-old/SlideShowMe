8f71790e1543e8d6e1b984fc114fd1c69af439a7
aicioara <primulandrei@yahoo.com>
2014-07-06 23:19:50 -0700

    About to copy code from the internet. Working state here

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,27 @@
 registredPages = {}
 
+var socket = io('http://10.0.0.36:3000');
+socket.on('yournumber', function(s) {
+	console.log(s);
+	for (var i in registredPages) {
+		if (registredPages[i] === "guest") {
+			console.log(i);
+			chrome.tabs.update(1, {url: "http://www.google.com"});
+		}
+	}
+
+})
+
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo && changeInfo.status == "complete") {
 
-    	console.log('caught');
-    	console.log("TabID " + tabId);
-    	console.log("Registred pages of TabID " +  registredPages[tabId])
+    	// console.log('caught');
+    	// console.log("TabID " + tabId);
+    	// console.log("Registred pages of TabID " +  registredPages[tabId])
 
     	if (registredPages[tabId] === "host") {
-    		console.log("host");
+    		// console.log("host");
 
 			chrome.browserAction.setIcon({
 				path: "icons/icon_green.png",
